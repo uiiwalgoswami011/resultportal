732b268b8d102b51923a67814a668ec5243735b7
Ujjwal Goswami <ujjwalgoswami011@gmail.com>
2025-12-22 19:15:54 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,61 +1,61 @@
 const resultsData = [
   {
     title: "Bihar Board 10th Result 2026",
-    link: "results/bihar-board-10th-result-2026.html",
+    link: "bihar-board-10th-result-2026.html",
     status: "Coming Soon",
     tag: "Board"
   },
   {
     title: "Bihar Board 12th Result 2026",
-    link: "results/bihar-board-12th-result-2026.html",
+    link: "bihar-board-12th-result-2026.html",
     status: "Coming Soon",
     tag: "Board"
   },
   {
     title: "CBSE 10th Result 2026",
-    link: "results/cbse-10th-result-2026.html",
+    link: "cbse-10th-result-2026.html",
     status: "Coming Soon",
     tag: "Board"
   },
   {
     title: "CBSE 12th Result 2026",
-    link: "results/cbse-12th-result-2026.html",
+    link: "cbse-12th-result-2026.html",
     status: "Coming Soon",
     tag: "Board"
   },
   {
     title: "UP Board 10th Result 2026",
-    link: "results/up-board-10th-result-2026.html",
+    link: "up-board-10th-result-2026.html",
     status: "Coming Soon",
     tag: "Board"
   },
   {
     title: "CUET UG Result 2026",
-    link: "results/cuet-ug-result-2026.html",
+    link: "cuet-ug-result-2026.html",
     status: "Coming Soon",
     tag: "Exam"
   },
   {
     title: "SSC GD Result 2026",
-    link: "results/sscgd.html",
+    link: "sscgd.html",
     status: "Coming Soon",
     tag: "Exam"
   },
   {
     title: "Railway Group D Result 2026",
-    link: "results/railway-group-d-result-2026.html",
+    link: "railway-group-d-result-2026.html",
     status: "Coming Soon",
     tag: "Exam"
   },
   {
     title: "CTET Result 2026",
-    link: "results/ctet-result-2026.html",
+    link: "ctet-result-2026.html",
     status: "Coming Soon",
     tag: "Exam"
   },
   {
     title: "JEEMAIN Result 2026",
-    link: "results/jeemain-result-2026.html",
+    link: "jeemain-result-2026.html",
     status: "Coming Soon",
     tag: "Exam"
   }
