bcf31268067c150f3a8a63aa09dd967160f8b451
Ujjwal Goswami <ujjwalgoswami011@gmail.com>
2025-12-24 14:04:00 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// ✅ Mobile menu toggle
+const menuBtn = document.getElementById('menuBtn');
+const navLinks = document.getElementById('navLinks');
+
+menuBtn.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
